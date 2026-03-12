a5007479a03d7da5f9cd9f39e35ab5037399052a
Bazzcuy <nanamigemoy@gmail.com>
2026-03-12 21:17:45 +0700

    refactor: remove unused Image import and trigger new build

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { NavBar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
